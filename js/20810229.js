d529c2bb8b12f4dac97b17e086c1bc49633af4f5
maidinhthaison <mdts1988@gmail.com>
2022-10-20 00:50:54 +0700

    binding data to blogs, loading comments

--- js/20810229.js
+++ js/20810229.js
@@ -10,3 +10,39 @@ async function loadData(request, templateId, viewId) {
   var view = document.getElementById(viewId);
   view.innerHTML = template(context);
 }
+
+async function loadBlogs(request, currentPage, templateId, viewId) {
+  const response = await fetch(`${API}/${request}?page=${currentPage}`);
+  const context = await response.json();
+  context.currentPage = currentPage;
+
+  var source = document.getElementById(templateId).innerHTML;
+  var template = Handlebars.compile(source);
+
+  var view = document.getElementById(viewId);
+  view.innerHTML = template(context);
+}
+
+async function loadBlogComments(
+  blogId,
+  templateId,
+  viewId,
+  gotoComment = false
+) {
+  await loadData(`blogs/${blogId}`, templateId, viewId);
+  if (gotoComment) {
+    window.location.href = "#comments";
+  }
+}
+
+Handlebars.registerHelper("formatDate", function (date) {
+  let formatDate = new Date(date);
+  let options = {
+    weekdays: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZonename: "short",
+  };
+  return formatDate.toLocaleDateString("en-US", options);
+});
